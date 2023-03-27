9e3d55ca0fa108b603e113cac8a19bc9cde3c516
Bautista <bautistabadino@gmail.com>
2023-03-27 20:19:28 -0300

    error detalle

--- src/Components/ItemDetail.jsx
+++ src/Components/ItemDetail.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
-export const ItemDetail = () => {
+export const ItemDetail = (item) => {
   const { id } = useParams()
+  console.log(item);
   return (
     <div>ItemDetail</div>
   )
--- src/Components/ItemDetailContainer.jsx
+++ src/Components/ItemDetailContainer.jsx
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { ItemDetail } from './ItemDetail'
 
 export const ItemDetailContainer = () => {
   const [item, setItem] = useState({})
   const {id} = useParams()
+ 
   useEffect(() => {
     const obtenerDatos = async () => {
-      const respuesta = await fetch('src/data/productos.json');
+      const respuesta = await fetch('../src/data/productos.json');
       const datosJson = await respuesta.json();
-      setItem(datosJson.find((obj) => obj.id === parseInt(id)));
+      const itemEncontrado = datosJson.find((producto) => producto.id === parseInt(id));
+      setItem(itemEncontrado);
     };
-
+    
     obtenerDatos();
-  }, []);
+  }, [id]);
 
   return (
-    <div>{id}</div>
+    <ItemDetail item={item} />
   )
 }

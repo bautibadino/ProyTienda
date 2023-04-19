2817ec34b24bafead83c9bd12d83b689ac4bd5b3
Bautista <bautistabadino@gmail.com>
2023-04-19 14:55:21 -0300

    adelantos detalles

--- src/Components/Cart/CheckOut.jsx
+++ src/Components/Cart/CheckOut.jsx
@@ -1,9 +1,11 @@
 import React, { useContext, useState, useEffect } from "react";
 import { Context } from "../../Context/Context";
-import { Card, Button, Container, Row, Col } from 'react-bootstrap';
+import { Card, Button, Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../../firebase/firebase";
+import { MDBBtn } from "mdb-react-ui-kit";
+
 
 export const CheckOut = () => {
   const cart = useContext(Context);
@@ -27,7 +29,7 @@ export const CheckOut = () => {
       total += totalItem;
     }
     setTotalCheckoutitems(total);
-    console.log(total);
+    return total;
   };
 
   const handleSubmit = (e) => {
@@ -40,20 +42,17 @@ export const CheckOut = () => {
       date: new Date(),
     };
     addDoc(ventas, newVenta);
-
   };
 
   useEffect(() => {
     handleCheckout();
-
   }, [cart]);
 
   return (
-    <div className="container-checkout">
-
-
-
+    // CONTAINER GENERAL CHECKOUT
+    <Container className="d-flex flex-row">
       <div className="productos-checkout">
+        {/* SI EL CARRITO TIENE 0 PRODUCTOS MUESTRA ESTO */}
         <h4>Tus productos</h4>
         {cart.cart.length === 0 ? (
           <>
@@ -62,95 +61,134 @@ export const CheckOut = () => {
               <Link to="/">regresar</Link>
             </MDBBtn>
           </>
-        ) : null}
-        <ul className="lista-productos">
-          {cart.cart.map((item) => (
-            <li className="product-checkout" key={item.id}>
-              <img src={item.url_imagen} alt="" />
-              <p>{item.cantidad}</p>
-              <p>{item.producto}</p>
-              <p>{item.precio}</p>
-              <p>
-                total :{" "}
-                {parseInt(item.precio.replace("$", "")) * item.cantidad}
-              </p>
-            </li>
-          ))}
-        </ul>
-      </div>
+        ) : (
+          // SI EL CARRITO TIENE MAS DE 0 PRODUCTOS MUESTRA LO SIGUIENTE
+          <div className="">
+            <div className="">
+              <div className="w-100 row align-items-start space-between">
+                <div className="d-flex align-items-center justify-content-center col w-25">
+                  <span></span>
+                </div>
+                <div className="d-flex align-items-center justify-content-center col w-25">
+                  <small>unidades</small>
+                </div>
+                <div className="d-flex align-items-center justify-content-center col w-25">
+                  <small>producto</small>
+                </div>
+                <div className="d-flex align-items-center justify-content-center col w-25">
+                  <small>precio</small>
+                </div>
+              </div>
+              <ul className="lista-productos">
+                {/* LISTA CARRO */}
+                {cart.cart.map((item) => (
+                  <li
+                    className="product-checkout d-flex justify-content-around align-items-center"
+                    key={item.id}
+                  >
+                    <img
+                      className="d-flex align-items-center justify-content-center w-25"
+                      src={item.url_imagen}
+                      alt=""
+                    />
+                    <small className="d-flex align-items-center justify-content-center w-25">
+                      {item.cantidad}
+                    </small>
+                    <small className="d-flex align-items-center justify-content-center flex-wrap w-25">
+                      {item.producto}
+                    </small>
+                    <small className="d-flex align-items-center justify-content-center w-25">
+                      ${item.precio}
+                    </small>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+            )}
+            <strong>TOTAL: ${totalCheckoutitems}</strong>
+          </div>
+      
+
+
       <div className="datos-checkout">
-        <h4>Datos de envio</h4>
-        <form>
+          <div className="">
+            <h4>Datos de envio</h4>
+            <form>
+              <div class="row">
+                <div class="col">
+                  <input
+                    onChange={handleChange}
+                    type="text"
+                    name="nombre"
+                    className="form-control"
+                    placeholder="Nombre"
+                    value={datos.nombre}
+                  />
+                </div>
+                <div class="col">
+                  <input
+                    onChange={handleChange}
+                    type="text"
+                    name="apellido"
+                    className="form-control"
+                    placeholder="Apellido"
+                    value={datos.apellido}
+                  />
+                </div>
+              </div>
 
-        <div class="row">
-    <div class="col">
-      <input
-              onChange={handleChange}
-              type="text"
-              name="nombre"
-              className="form-control"
-              placeholder="Nombre"
-              value={datos.nombre}
-            />
-    </div>
-    <div class="col">
-      <input
-              onChange={handleChange}
-              type="text"
-              name="apellido"
-              className="form-control"
-              placeholder="Apellido"
-              value={datos.apellido}
-            />
-    </div>
-  </div>
+              <input
+                onChange={handleChange}
+                type="text"
+                name="direccion"
+                className="form-control"
+                placeholder="Direccion"
+                value={datos.direccion}
+              />
+              <input
+                onChange={handleChange}
+                type="text"
+                name="ciudad"
+                className="form-control"
+                placeholder="Ciudad"
+                value={datos.ciudad}
+              />
+              <input
+                onChange={handleChange}
+                type="text"
+                name="provincia"
+                className="form-control"
+                placeholder="Provincia"
+                value={datos.provincia}
+              />
+              <input
+                onChange={handleChange}
+                type="text"
+                name="codigoPostal"
+                className="form-control"
+                placeholder="Codigo Postal"
+                value={datos.codigoPostal}
+              />
+              <input
+                onChange={handleChange}
+                type="text"
+                name="email"
+                className="form-control"
+                placeholder="Email"
+                value={datos.email}
+              />
 
-          <input
-            onChange={handleChange}
-            type="text"
-            name="direccion"
-            className="form-control"
-            placeholder="Direccion"
-            value={datos.direccion}
-          />
-          <input
-            onChange={handleChange}
-            type="text"
-            name="ciudad"
-            className="form-control"
-            placeholder="Ciudad"
-            value={datos.ciudad}
-          />
-          <input
-            onChange={handleChange}
-            type="text"
-            name="provincia"
-            className="form-control"
-            placeholder="Provincia"
-            value={datos.provincia}
-          />
-          <input
-            onChange={handleChange}
-            type="text"
-            name="codigoPostal"
-            className="form-control"
-            placeholder="Codigo Postal"
-            value={datos.codigoPostal}
-          />
-          <input
-            onChange={handleChange}
-            type="text"
-            name="email"
-            className="form-control"
-            placeholder="Email"
-            value={datos.email}
-          />
-
-          <button onClick={() => (handleCheckout, handleSubmit)} type="button" className="btn btn-primary">
-            Comprar
-          </button>
-        </form>
-      </div>
-    </div>
+              <button
+                onClick={() => (handleCheckout, handleSubmit)}
+                type="button"
+                className="btn btn-primary"
+              >
+                Comprar
+              </button>
+            </form>
+          </div>
+        </div>
+    </Container>
   );
 };
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,19 +7,19 @@ import { Nav } from "react-bootstrap";
 export const Navbar = () => {
 
   return (
-    <nav className="animate__animated animate__bounce navbar navbar-expand-sm navbar-dark bg-dark">
+    <nav className="animate__animated animate__bounce navbar navbar-expand-sm navbar navbar-dark bg-dark">
       <div className="container-fluid">
         <div className="navbar-brand">
           <NavLink 
           to={'/'}
-          className= {'brand h2 text-decoration-none text-white'}
+          className= {'navbar-brand link'}
           >
             Tienda</NavLink>
         </div>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <NavLink
             className={({ isActive }) =>
-              `nav-item nav-link link ${isActive ? "active" : ""}`
+              `nav-item nav-link  ${isActive ? "active" : ""}`
             }
             to="/iphone"
           >

d70bb2932f690f2d08207244889d006c7bf7aad5
Bautista <bautistabadino@gmail.com>
2023-06-30 19:18:51 +0200

    fix

--- src/Components/ItemDetail/ItemDetail.jsx
+++ src/Components/ItemDetail/ItemDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { ItemQuantitySelector } from "./itemQuantitySelector";
+import { ItemQuantitySelector } from "./ItemQuantitySelector";
 import { Context } from "../../Context/Context";
 
 
